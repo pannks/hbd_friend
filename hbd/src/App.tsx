91e096737623dfb45bb62e6405e4a5f7f22b224f
Pann Kaansadich <pannrattanapong@gmail.com>
2023-04-05 18:44:16 +0700

    new structure files-1

--- hbd/src/App.tsx
+++ hbd/src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <div className="App">
             <img src="/src/images/hbd_ink_23.jpg" width={500} alt="" />
