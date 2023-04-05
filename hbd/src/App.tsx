5ad81bb8b9dcfd7e39957921a738ccf9b98694c4
Pann Kaansadich <pannrattanapong@gmail.com>
2023-04-05 18:49:11 +0700

    new structure files-2

--- hbd/src/App.tsx
+++ hbd/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <img src="/src/images/hbd_ink_23.jpg" width={500} alt="" />
+            <img src="/images/hbd_ink_23.jpg" width={500} alt="" />
             <h1>Happy Birthday Ja!</h1>
             <p>
                 มีความสุขมากๆ พี่อิ้งก็เก่งอยู่แล้ว ขอให้เก่งๆ
